c9481bc3fd78a802f7c80c3c81580e470e8ee051
Naman Gupta <naman22forever@gmail.com>
2014-03-25 17:43:14 +0530

    -used set instead of map.

--- WordGame.cpp
+++ WordGame.cpp
@@ -5,6 +5,7 @@
 #include<fstream>
 #include<string>
 #include<unordered_map>
+#include<set>
 #include<list>
 
 
@@ -44,54 +45,58 @@ int main()
 
     unordered_map <string, list<string>  > dict_map;
 
-    if(buildhashmap(dict_map))
+    if (!buildhashmap(dict_map))
     {
+        cout<< "Unable to open file";
+        return 0;
+    }
 
-        cout<<"Map Built...\n";
+    cout<<"Map Built...\n";
 
-        unordered_map <string, string> wordlist;
-        char str_imp,str_input[7];
-        int counter = 1<<6;
+    set<string> wordlist;
 
+    char str_imp,str_input[7];
+    int counter = 1<<6;
 
+    cout<<"Enter 1+6 alphabets, fixed alphabet first ";
+    cin >> str_imp >>str_input;
 
-        cout<<"Enter 1+6 alphabets, fixed alphabet first ";
-        cin >> str_imp >>str_input;
+    cout<<"\n\n   Found Words:- \n\n";
 
-        cout<<"\n\n   Found Words:- \n\n";
-
-        for(int i = 1 ; i<counter ; i++)
+    for(int i = 1 ; i<counter ; i++)
+    {
+        int pos = 0;
+        string lookup;
+        lookup.push_back(str_imp);
+        int j = i;
+        while(j)
         {
-            int pos = 0;
-            string lookup;
-            lookup.push_back(str_imp);
-            int j = i;
-            while(j)
-            {
-                if( j&1 == 1)
-                    lookup.push_back(str_input[pos]);
-                pos++;
-                j>>=1;
-            }
+            if( j&1 == 1)
+                lookup.push_back(str_input[pos]);
+            pos++;
+            j>>=1;
+        }
 
-            if(lookup.length()>=4)
-            {
-                sort(lookup.begin(),lookup.end());
+        if(lookup.length()>=4)
+        {
+            sort(lookup.begin(),lookup.end());
 
-                if(dict_map.count(lookup)>0)
+            if(dict_map.count(lookup)>0)
+            {
+                list <string> word_list= dict_map[lookup];
+                for(auto itr = word_list.begin() ; itr!=word_list.end(); itr++)
                 {
-                    list <string> word_list= dict_map[lookup];
-                    for(auto itr = word_list.begin() ; itr!=word_list.end(); itr++)
-                        wordlist[*itr] = *itr;
+                    if( wordlist.find(*itr) == wordlist.end() )
+                        wordlist.insert(*itr);
+
                 }
             }
         }
-
-        for(auto itr = wordlist.begin() ; itr!=wordlist.end() ; itr++)
-            cout<<itr->first<<endl;
     }
-    else
-        cout<< "Unable to open file";
+
+    for(auto itr = wordlist.begin() ; itr!=wordlist.end() ; itr++)
+        cout<<*itr<<endl;
+
 
     return 0;
 }
